65f420914fbfba999a19af94147664ff3a6c7e6c
Manish Kumar Sharma <78640018+Msharma673@users.noreply.github.com>
2025-11-13 21:53:58 +0530

    update

---
+++ SpringBootSecurityAuthenticationAndAuthorization/src/main/java/com/SpringBootSecurity/SpringBootSecurityAuthenticationAndAuthorization/SpringBootSecurityAuthenticationAndAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootSecurity.SpringBootSecurityAuthenticationAndAuthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityAuthenticationAndAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityAuthenticationAndAuthorizationApplication.class, args);
+	}
+
+}
---
+++ SpringBootSecurityAuthenticationAndAuthorization/src/test/java/com/SpringBootSecurity/SpringBootSecurityAuthenticationAndAuthorization/SpringBootSecurityAuthenticationAndAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootSecurity.SpringBootSecurityAuthenticationAndAuthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityAuthenticationAndAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
